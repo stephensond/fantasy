721e869fa39299b9559f48a93ca960aae8c6c324
Cheesywizz <70651459+Cheesywizz@users.noreply.github.com>
2021-06-25 14:59:36 -0400

    Update newdraft.sql

--- db/newdraft.sql
+++ db/newdraft.sql
@@ -1,40 +1,30 @@
-UPDATE players SET isdrafted = false;
-INSERT INTO leagues
-    (ID, team1, team2, team3, team4)
-SELECT MAX(l.ID)+1, MAX(t.teamID)+1, MAX(t.teamID)+2, MAX(t.teamID)+3, MAX(t.teamID)+4
-FROM leagues AS l JOIN teams AS t ON l.ID = t.leagueID;
-INSERT INTO teams
-    (teamId, leagueid)
-    SELECT team1, id
-    FROM leagues
-    WHERE ID = (SELECT MAX(ID)
-    FROM leagues)
-UNION
-    SELECT team2, id
-    FROM leagues
-    WHERE ID = (SELECT MAX(ID)
-    FROM leagues)
-UNION
-    SELECT team3, id
-    FROM leagues
-    WHERE ID = (SELECT MAX(ID)
-    FROM leagues)
-UNION
-    SELECT team4, id
-    FROM leagues
-    WHERE ID = (SELECT MAX(ID)
-    FROM leagues);
-DROP TABLE IF EXISTS draftroom;
-CREATE TABLE draftroom
-(
-    playerid int,
-    playername varchar(50),
-    pos varchar(3),
-    team varchar(30),
-    isDrafted INT,
-    teamID INT NULL
-);
-INSERT INTO draftroom
-    (playerid, playername, pos, team, isDrafted, teamID)
-SELECT playerid, playername, pos, team, 0, NULL
-FROM players;
+CREATE OR REPLACE FUNCTION NewDraft()
+RETURNS TABLE(teamid int)
+LANGUAGE plpgsql
+AS $$ 
+
+	declare
+        max_league_id int;
+        num_teams int ;
+	
+    -- create a default new league
+	BEGIN
+	    insert into leagues
+        (datecreated)
+        values
+        (current_timestamp);
+	
+	    max_league_id := (select MAX(leagueid) from leagues);
+	    num_teams := (select numteams from leagues where leagueid = max_league_id);
+	
+        -- add enough teams as per league size
+        for counter in 1..num_teams loop
+            insert into teams (leagueid)
+            values (max_league_id);
+        end loop;
+
+        -- return relevant teamIDs for this draft
+	    return query
+		    select ID from teams where leagueid = max_league_id;
+    END
+$$
