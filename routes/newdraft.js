f4b32dba14d211bca4aa663e38cb0db5b774fa58
Cheesywizz <70651459+Cheesywizz@users.noreply.github.com>
2021-06-26 00:31:02 -0400

    Update newdraft.js

--- routes/newdraft.js
+++ routes/newdraft.js
@@ -1,26 +1,11 @@
 import db from '../db/index.js';
 import express from 'express';
 const router = express.Router();
-import * as fs from 'fs';
-import path  from 'path';
 
-var sqlstring = path.join(path.resolve(), 'db', 'newdraft.sql');
-
-let query = "";
-fs.readFile(sqlstring, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err)
-    }
-    query = data;
-})
-
-router.post('/', function (req, res) {
-    db.query(query, [])
-        .then(results => res.status(200))
+router.get('/', function (req, res) {
+    db.query('SELECT * from NewDraft();', [])
+        .then(results => res.send(results.rows))
         .catch(err => console.error(err.stack));
 });
 
 export default router;
-
-
-
