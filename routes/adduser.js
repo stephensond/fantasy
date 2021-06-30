f69261b24e18815d5f016aada33977db1b1c0c1b
Alex <alex.wisnaskas@gmail.com>
2021-06-30 15:44:27 -0400

    testing user addition

--- routes/adduser.js
+++ routes/adduser.js
@@ -5,13 +5,9 @@ const router = express.Router();
 router.post('/', function (req, res) {
     const body = req.body;
         db.query('INSERT INTO Users (Username, Password) VALUES ($1, $2)'
-        , [body.username, body.pass], (err, result) => {
-            if (err) {
-                res.send('Username taken')
-            }
-        })
+        , [body.username, body.pass])
         .then(result => res.status(200).send('Account successfully created'))
-        .catch(err => console.error(err.stack));
+        .catch(err => res.status(400).send('Username taken', err.stack));
 });
 
 export default router;
