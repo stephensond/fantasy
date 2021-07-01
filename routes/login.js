187dac23793fd77c6cf2afbcf11a4b61c38e64b5
Alex <alex.wisnaskas@gmail.com>
2021-07-01 14:38:00 -0400

    login/logout

--- routes/login.js
+++ routes/login.js
@@ -4,10 +4,11 @@ const router = express.Router();
 
 router.post('/', function (req, res) {
     const body = req.body;
-        db.query('SELECT UserID FROM Users WHERE username = $1 AND password = $2'
+        db.query('SELECT Username FROM Users WHERE username = $1 AND password = $2'
         , [body.username, body.pass])
-        .then(result => result.rows.length)
-        .then(len => (len === 1) ? res.status(200).send() : res.status(400).send())
+        .then(result => result.rows)
+        .then(rows => (rows.length === 1) ? res.status(200).send(rows[0])
+                                          : res.status(400).send())
         .catch(err => console.log(err));
 });
 
