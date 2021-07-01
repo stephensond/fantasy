7ad76db7d882039f69cc577c6ed265c67a708dfa
Alex <alex.wisnaskas@gmail.com>
2021-07-01 13:46:26 -0400

    login box

--- routes/login.js
+++ routes/login.js
@@ -6,7 +6,7 @@ router.post('/', function (req, res) {
     const body = req.body;
         db.query('SELECT UserID FROM Users WHERE username = $1 AND password = $2'
         , [body.username, body.pass])
-        .then(result => {console.log(result.rows.length); return result.rows.length})
+        .then(result => result.rows.length)
         .then(len => (len === 1) ? res.status(200).send() : res.status(400).send())
         .catch(err => console.log(err));
 });
