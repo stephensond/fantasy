4cbfe006340949ad1043ab9b4875f5220bf92d74
Alex <alex.wisnaskas@gmail.com>
2021-07-01 12:58:37 -0400

    login route

---
+++ routes/login.js
@@ -0,0 +1,14 @@
+import db from '../db/index.js';
+import express from 'express';
+const router = express.Router();
+
+router.post('/', function (req, res) {
+    const body = req.body;
+        db.query('SELECT UserID FROM Users WHERE username = $1 AND password = $2'
+        , [body.username, body.pass])
+        .then(result => (result.length === 1) ? res.status(200) : res.status(400))
+        .then(res.send())
+        .catch(err => console.error(err.stack));
+});
+
+export default router;
