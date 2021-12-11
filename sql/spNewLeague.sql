06274744a1fe3a4e22ef3ffcb5b0137b9af153d2
Cheesywiz <awisnaskas@gmail.com>
2021-12-11 17:21:06 -0500

    proc fix

--- sql/spNewLeague.sql
+++ sql/spNewLeague.sql
@@ -2,15 +2,12 @@ CREATE OR REPLACE FUNCTION NewLeague(Params json)
 LANGUAGE plpgsql
 AS $$ 
 
-	declare
-        max_league_id int;
-        num_teams int;
-	
     -- create a default new league
 	BEGIN
 
         INSERT INTO Leagues 
         (LeagueName, OwnerUserName, datecreated, numteams, numplayers)
+        SELECT
             (rec->>'leaguename')::text,
             (rec->>'leagueowner')::text,
             current_timestamp as datecreated,
