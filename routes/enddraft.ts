f2d0694d23477bec680e6ac4c74959108d2d6411
David Stephenson <stephenson.d@northeastern.edu>
2021-12-11 13:43:16 -0500

    fix end draft

--- routes/enddraft.ts
+++ routes/enddraft.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post('/', function (req, res) {
     const body = req.body;
     db.query('CALL endDraft($1)', [body])
-        .then(result => res.status(200).send(body.id.toString()))
+        .then(result => res.status(200).send('success'))
         .catch(err => console.error(err.stack));
 });
 
